3b2892ce36beee40f7c8deead4c522ef4a8b55c3
Klonek <66069326+KlonekV5@users.noreply.github.com>
2024-04-13 19:52:25 +0200

    Add files via upload

--- lab6-django/polls/models.py
+++ lab6-django/polls/models.py
@@ -19,9 +19,6 @@ class Choice(models.Model):
 class Token(models.Model):
     value = models.CharField(max_length=200, default=uuid.uuid4, unique=True)
     used = models.BooleanField(default=False)
-    relation = models.ManyToManyField(Choice, blank=True)
 
     def __str__(self):
         return str(self.value) + ';' + str(self.used)
-
-#https://docs.djangoproject.com/en/5.0/ref/models/fields/#django.db.models.ManyToManyField
--- lab6-django/polls/views.py
+++ lab6-django/polls/views.py
@@ -20,48 +20,47 @@ def answers(request):
 
 def poll(request):
     token = request.GET.get('Token')
-
     if not token:
-        token = Token.objects.create(value=str(uuid.uuid4()), used=False)
-        request.session['Token'] = token.value
+        return HttpResponse('Token is missing')
 
-
-    session_token = request.session.get('Token')
-    if not session_token:
-        return HttpResponse('Error: no token')
-    token = Token.objects.get(value=session_token)
-    if not token.used:
-        latest_question_list = Question.objects.all()
-        template = loader.get_template('polls/poll.html')
-        context = {
-            'latest_question_list': latest_question_list,
-            'token' : token
-        }
-        return HttpResponse(template.render(context, request))
-    elif token.used:
-        return HttpResponse('Error: Token already used')
     else:
-        return HttpResponse('Unknown Error!')
+        try:
+            token = Token.objects.get(value=token)
+            # token = Token.objects.get_or_create(value=token, used=False)[0] #tokens created 0-99  ?Token=0
+        except:
+            return HttpResponse('Token is invalid')
+
+        if not token.used:
+            latest_question_list = Question.objects.all()
+            template = loader.get_template('polls/poll.html')
+            context = {
+                'latest_question_list': latest_question_list,
+                'token': token,
+            }
+            return HttpResponse(template.render(context, request))
+
+        elif token.used:
+            return HttpResponse('Token is already used')
 
 
 def save(request):
     if request.method == 'GET':
         token = request.GET.get('Token')
-        if token:
-            token = Token.objects.get(value=token)
-            if not token.used:
-                for item in request.GET:
-                    if item != 'csrfmiddlewaretoken':
-                        question = Question.objects.get(question_text=item)
-                        selected_choice = Choice.objects.get(choice_text=request.GET[item], question=question.id)
-                        selected_choice.votes += 1
-                        selected_choice.save()
-                        token.used = True
-                        token.save()
-                return HttpResponse('Answers saved!')
-            elif token.used:
-                return HttpResponse('Error: Token already used')
-            else:
-                return HttpResponse('Unknown Error!')
+        if not token:
+            return HttpResponse('Token is missing')
+
+        token = Token.objects.get(value=token)
+        if not token.used:
+            for item in request.GET:
+                if item != 'csrfmiddlewaretoken' and item != 'Token':
+                    question = Question.objects.get(question_text=item)
+                    selected_choice = Choice.objects.get(choice_text=request.GET[item], question=question.id)
+                    selected_choice.votes += 1
+                    selected_choice.save()
+            token.used = True
+            token.save()
+            return HttpResponse('Answers saved!')
         else:
-            return HttpResponse('Error: No Token')
+            return HttpResponse('Cannot save answer, token is already used')
+    else:
+        return HttpResponse('Invalid request method')
